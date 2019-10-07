7e9ac3cfda4d7b1af4accc73cd5403c30f1e4ba0
artacab <artacab@yandex.ru>
2019-10-07 21:55:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  for (let item = 0; item < str.length; item++) {
+      const char = str[item];
+      for (let index = 0; index < bracketsConfig.length; index++) {
+          const template = bracketsConfig[index];
+          if (template.includes(char)) {
+              const lastElem = stack[stack.length - 1];
+              if (lastElem === template[0] && char === template[1]) {
+                stack.pop();
+              } else {
+                  stack.push(char);
+              }
+          }
+      }
+  }
+  return stack.length > 0 ? false : true;
+};
